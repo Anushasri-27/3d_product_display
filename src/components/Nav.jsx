fbfb00ee6ce44b58f0e2e666620ff32ea021b8d7
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-11-01 18:04:52 +0530

    3d product display app using react and gsap

---
+++ src/components/Nav.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Logo from "../assets/images/logo.svg";
+import store from "../assets/images/store.svg";
+import search from '../assets/images/search.svg';
+const Nav = () => {
+  return (
+    <nav className="nav-wrapper">
+      <div className="nav-content">
+        <ul className="list-styled">
+          <li>
+            <img src={Logo} alt="apple" />
+          </li>
+          <li>
+            <a className="link-styled">Store</a>
+          </li>
+          <li>
+            <a className="link-styled">Mac</a>
+          </li>
+          <li>
+            <a className="link-styled">ipad</a>
+          </li>
+          <li>
+            <a className="link-styled">iphone</a>
+          </li>
+          <li>
+            <a className="link-styled">watch</a>
+          </li>
+          <li>
+            <a className="link-styled">Airpods</a>
+          </li>
+          <li>
+            <a className="link-styled">Tv & Home</a>
+          </li>
+          <li>
+            <a className="link-styled">Entertainment</a>
+          </li>
+          <li>
+            <a className="link-styled">Accessories</a>
+          </li>
+          <li>
+            <a className="link-styled">Support</a>
+          </li>
+          <li>
+            <a className="link-styled">Store</a>
+          </li>
+          <li>
+            <img src={search} alt="search"/>
+          </li>
+          <li>
+            <img src={store} alt="store"/>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
